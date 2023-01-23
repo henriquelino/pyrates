44878889a3a9104d77ec7a3258d826bc23875590
henrique lino <henrique.lino97@gmail.com>
2023-01-23 09:23:16 -0300

    updated: now loads level0

--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
 import sys
 import pygame
-from settings import screen_width, screen_height, frame_rate, level_map
+from settings import screen_width, screen_height, frame_rate
 from level import Level
+from game_data import level0
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-level = Level(level_map, screen)
+level = Level(level0, screen)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit() 
+            pygame.quit()
             sys.exit()
 
-    screen.fill('black')
+    screen.fill('gray')
 
     level.run()
 
